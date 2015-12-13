a47e5687bf5bce9f0e5a1400c28bf07cfb5942d4
Garrett Frank Sickles <garrettsickles@gmail.com>
2015-12-12 22:26:30 -0700

    Donezo

--- Final Project/Final_Project.m
+++ Final Project/Final_Project.m
@@ -1,84 +1,48 @@
-function Final_Project()
-    % Get Filenames
-    playedFile = uigetfile('*.*','Played audio file');
-    recordFile = uigetfile('*.*','Recorded audio file');
-    inputFile = uigetfile('*.*','Simulate audio file');
-    
+function Final_Project(title)
     % Import the data
-    rawPlayed = importdata(playedFile);
-    rawRecord = importdata(recordFile);
-    rawInput = importdata(inputFile);
-    
-    % Check the data sampling frequencies
-    disp('Sampling frequencies of the data')
-    disp(['Played:   Fs = ', num2str(rawPlayed.fs)]);
-    disp(['Recorded: Fs = ', num2str(rawRecord.fs)]);
-    disp(['Simulate: Fs = ', num2str(rawInput.fs)]);
-    
+    junk1 = importdata(uigetfile('*.*','Played audio file'));
+    junk2 = importdata(uigetfile('*.*','Recorded audio file'));
+    junk3 = importdata(uigetfile('*.*','Simulate audio file'));
+    rawPlayed = junk1.data(:,1);
+    RPFS = junk1.fs;
+    rawRecord = junk2.data(:,1);
+    RRFS = junk2.fs;
+    rawInput = junk3.data(:,1);
+    RIFS = junk3.fs;
     % Fix the played audio
     i = 1;
-    d = rawPlayed.data(i);
+    d = rawPlayed(i);
     while d == 0
         i = i + 1;
-        d = rawPlayed.data(i);
+        d = rawPlayed(i);
     end
-    rawPlayed.data = rawPlayed.data(i:end);
-    
+    rawPlayed = rawPlayed(i:end);
     % Fix the data sampling frequencies
-    
-    record = struct('data', rawRecord.data(:,1), 'fs', rawRecord.fs);
-    
-    [NP, DP] = rat(rawRecord.fs./rawPlayed.fs);
-    played = struct('data', resample(rawPlayed.data(:,1), NP, DP), 'fs', rawRecord.fs);
-    
-    [NI, DI] = rat(rawRecord.fs./rawInput.fs);
-    input = struct('data', resample(rawInput.data(:,1), NI, DI), 'fs', rawRecord.fs);
-    
+    Fs = RPFS;
+    [NP, DP] = rat(Fs./RPFS);
+    played = struct('data', resample(rawPlayed(:,1), NP, DP), 'fs', Fs);
+    [NI, DI] = rat(Fs./RIFS);
+    input = struct('data', resample(rawInput(:,1), NI, DI), 'fs', Fs);
+    [NR, DR] = rat(Fs./RRFS);
+    record = struct('data', resample(rawRecord(:,1), NR, DR), 'fs', Fs); 
     % Deconvolve the signal
-    RL = length(record.data);
-    PL = length(played.data);
-    SL = length(input.data);
-    N = max(RL, PL, SL);
-    thing1 = fft(record.data, N)./fft(played.data, N);
-    filter = struct('data',ifft(fft(record.data, N)./fft(played.data, N)), 'fs', rawRecord.fs);
-    
+    N = max([length(record.data) length(played.data) length(input.data)]);
+    filter = struct('data',ifft(fft(record.data, N)./fft(played.data, N)), 'fs', record.fs);
+    % Band Pass 
+    filter.data = bandPass(filter.data, filter.fs, 20.0, 2000.0);
+    % Plot the the played
+    time2freq(played.data, played.fs, 'Played Frequency Sweep');
+    % Plot the the recorder
+    time2freq(record.data, record.fs, ['Recorded in ', title]);
+    % Plot the the input
+    time2freq(input.data, input.fs, 'Input: Mr. Mackey');
     % Plot the the filter
-    figure('position', [0, 0, 800, 450]);
-    subplot(2, 1, 1);
-    plot(Tn(filter.data, filter.fs, 0.0), filter.data);
-    title('Time Domain Response of the Filter');
-    xlabel('Time (sec)');
-    ylabel('Amplitude');
-    axis tight;
-    subplot(2, 1, 2);
-    plot(Fk(Tn(filter.data, fliter.fs, 0.0)), thing1);
-    title('Frequency Domain Response of the Filter');
-    xlabel('Frequency (Hz)');
-    ylabel('Amplitude');
-    axis tight;
-    
-    % Save the filter to a file
-    audiowrite('filter.wav', filter.data, filter.fs);
-    
+    time2freq(filter.data, filter.fs, ['Filter in ', title]);
     % Simulate a noise through the filter
-    thing2 = fft(input.data, N).*fft(filter.data, N);
-    output = struct('data', ifft(thing2), 'fs', filter.fs);
-    
+    output = struct('data', ifft(fft(input.data, N).*fft(filter.data, N)), 'fs', filter.fs);
     % Plot the the sound
-    figure('position', [0, 0, 800, 450]);
-    subplot(2, 1, 1);
-    plot(Tn(output.data, output.fs, 0.0), filter.data);
-    title('Time Domain Response of the Simulated Noise');
-    xlabel('Time (sec)');
-    ylabel('Amplitude');
-    axis tight;
-    subplot(2, 1, 2);
-    plot(Fk(Tn(output.data, output.fs, 0.0)), thing2);
-    title('Frequency Domain Response of the Simulated Noise');
-    xlabel('Frequency (Hz)');
-    ylabel('Amplitude');
-    axis tight;
-    
+    output.data = output.data ./ max(output.data);
+    time2freq(output.data, output.fs, [' Mr. Mackey in ', title]);
     % Write the simulated output
     audiowrite('Output.wav', output.data, output.fs);
 end
@@ -94,4 +58,31 @@ function [result] = Fk(times)
     Ts = (times(N) - times(1)) / (N - 1);
     dF = 1 / (N * Ts);
     result = ((0:N-1) - ceil(N/2))*dF;
+end
+
+function [result] = bandPass(input, fs, fmin, fmax)
+    freq = Fk(Tn(input, fs, 0.0));
+    filterFFT = fftshift(fft(input));
+    filterFFT(abs(freq) < fmin) = 0.0;
+    filterFFT(abs(freq) > fmax) = 0.0;
+    result = ifft(fftshift(filterFFT));
+end
+
+% Plot a time and frequency response of a data set
+function [result] = time2freq(input, fs, name)
+    result = fftshift(fft(input));
+    time = Tn(input, fs, 0.0);
+    figure('position', [0, 0, 800, 450]);
+    subplot(2, 1, 1);
+    plot(time, input);
+    title(['Time Domain Response of the ', name]);
+    xlabel('Time (sec)');
+    ylabel('Amplitude');
+    axis tight;
+    subplot(2, 1, 2);
+    plot(Fk(time), result);
+    title(['Frequency Domain Response of the ', name]);
+    xlabel('Frequency (Hz)');
+    ylabel('Amplitude');
+    axis tight;
 end
